26e221511390039d90208d0dd1ee70102915cac1
k1ng <k1ng@glass.earth>
2021-03-18 00:10:51 +0000

    dude

--- dockerfile.sh
+++ dockerfile.sh
@@ -3,6 +3,5 @@ export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
 
 echo "I am running on $BUILDPLATFORM, building for $TARGETPLATFORM" && \
     echo "working directory: $(pwd)" && ls -la && \
-    npm i -C /root/angular/works && \
-    npm i -C /root/nest/works && \
-    npm i -C /root/nx/works
+    echo "npm version: $(npm version)" && \
+    npm i -C ./angular/works && echo "finshed angular install"
