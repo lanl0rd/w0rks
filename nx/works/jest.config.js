fc1cedc44a63c20c133e41dc037d26ff92abd687
lanl0rd <k1ng@glass.earth>
2021-03-27 10:34:15 -0400

    works apps

---
+++ nx/works/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/works-nest',
+    '<rootDir>/apps/works-angular',
+    '<rootDir>/apps/works-react',
+    '<rootDir>/apps/works-express',
+    '<rootDir>/apps/works-web',
+    '<rootDir>/apps/works-node',
+  ],
+};
